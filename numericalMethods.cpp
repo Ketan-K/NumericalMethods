b10000dd1401f0eb0de8011b1ee920f0575ecec4
Ketan-K <ketankatore3367@gmail.com>
2019-04-28 15:59:16 +0530

    Assignment

--- numericalMethods.cpp
+++ numericalMethods.cpp
@@ -12,10 +12,9 @@ int main()
     cout << "\nRoot [ Newton Raphson ] :" << p.newtonRaphson();
     cout << "\nRoot [ Secant Method ] :" << p.secantMethod();
 
-    /*
     cout << "\nRoot [ Bisection Method ]: " << p.bisection();
     cout << "\nRoot [ Regula false Method ]: " << p.regulafalse();
-    */
+
     cout << endl;
     return 1;
 }
