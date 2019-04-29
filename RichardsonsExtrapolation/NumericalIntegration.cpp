0877799814e4042cb241c6bb6e1b7fea4cffff82
Ketan-K <ketankatore3367@gmail.com>
2019-04-29 16:51:34 +0530

    Numerical Integration

--- RichardsonsExtrapolation/NumericalIntegration.cpp
+++ RichardsonsExtrapolation/NumericalIntegration.cpp
@@ -19,7 +19,7 @@ float simpson13(float a, float b, int n) // n is even
 {
     if (n % 2 != 0)
     {
-        cerr << "Simpsons 1/3 rule cannot be applied, N needs to be even.";
+        cerr << "Simpsons 1/3 rule cannot be applied, N should be an even number.";
         return 0;
     }
     float h = (b - a) / n;
@@ -35,8 +35,25 @@ float simpson13(float a, float b, int n) // n is even
     return sum;
 }
 
-float bools(float a, float b, int n)
+float bools(float a, float b, int n) //n is multiple of 4
 {
+    if (n % 4 != 0)
+    {
+        cerr << "Bool\'s rule cannot be applied . N should be multiple of 4.";
+        return 0;
+    }
+    float h = (b - a) / n;
+    float sum = (14 * h / 45) * (f(a) + f(b));
+    for (int i = 1; i < n; i++)
+    {
+        if (i % 4 == 0)
+            sum += (2 * h / 45) * (14 * f(a + i * h));
+        else if (i % 2 == 0)
+            sum += (2 * h / 45) * (12 * f(a + i * h));
+        else
+            sum += (2 * h / 45) * (32 * f(a + i * h));
+    }
+    return sum;
 }
 int main()
 {
@@ -44,8 +61,9 @@ int main()
     int nodes = 6;
     cout << "Enter Interval : ";
     cin >> a >> b;
-    /* cout << "\nHow many nodes : ";
-    cin >> nodes;*/
+    cout << "\nHow many nodes : ";
+    cin >> nodes;
     cout << "\nTrapezoidal Rule : " << trapezoidal(a, b, nodes) << endl;
     cout << "Simpsons 1/3 Rule : " << simpson13(a, b, nodes) << endl;
+    cout << "Bool's Rule : " << bools(a, b, nodes) << endl;
 }
